bd53e0c8b4bcacb8e8f056ac0e062b4ffff4b8af
Julanna11 <joy93julanna@gmail.com>
2022-05-16 15:44:36 +0300

    2nd try

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,23 +5,21 @@ const throttle = require("lodash.throttle");
 const STORAGE_KEY = 'feedback-form-state';
 
 const formData = {};
-
         
 const form = document.querySelector('.feedback-form');
-const email = document.querySelector('.feedback-form input');
-const message = document.querySelector('.feedback-form textarea');
+// const email = document.querySelector('.feedback-form input');
+// const message = document.querySelector('.feedback-form textarea');
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormData, 500));
-
-
-
 
 function onFormData(e) {
   formData[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }; 
+
 populateForm();
+
 function onFormSubmit(e) {
   console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
   e.preventDefault();
